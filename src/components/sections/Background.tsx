659084381c3ec1b7502c0206057168caf87239b2
Dylan <dms@soliduscapital.io>
2024-04-26 19:38:54 -0600

    chg | background

--- src/components/sections/Background.tsx
+++ src/components/sections/Background.tsx
@@ -17,16 +17,15 @@ import avanttia from '../../assets/avanttia.png';
 import infosapiens from '../../assets/infosapiens.jpg';
 import solidus from '../../assets/solidus.jpg';
 
-
 function Technologies() {
   return (
     <Card>
       <span className="text-3xl font-bold text-center">Background</span>
       <span className="text-base font-bold text-center flex gap-4 md:text-2xl">
-        I'm a Full Stack Developer with 4+ years of experience in the industry.
+        I'm a Full Stack Developer with 5+ years of experience in the industry.
       </span>
       <span className="text-base font-bold text-center flex gap-4 md:text-2xl">
-      Here are some of the technologies I've been working with:
+        Here are some of the technologies I've been working with:
       </span>
       <div className="flex flex-wrap justify-center gap-4 md:gap-12">
         <PhotoCarousel photos={[reactLogo, serverless, angular]} />
@@ -37,9 +36,9 @@ function Technologies() {
         And here are some companies I've worked with:
       </span>
       <div className="flex flex-wrap justify-center gap-4 md:gap-12">
-        <CustomImage title='Avanttia' photo={avanttia} url='https://www.avanttia.com/'/>
-        <CustomImage title='Infosapiens' photo={infosapiens} url='https://www.infosapiens.mx/'/>
-        <CustomImage title='Solidus Capital' photo={solidus} url='https://www.soliduscapital.io/'/>
+        <CustomImage title="Avanttia" photo={avanttia} url="https://www.avanttia.com/" />
+        <CustomImage title="Infosapiens" photo={infosapiens} url="https://www.infosapiens.mx/" />
+        <CustomImage title="Solidus Capital" photo={solidus} url="https://www.soliduscapital.io/" />
       </div>
     </Card>
   );
